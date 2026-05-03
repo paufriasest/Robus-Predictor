86490720c583c2825b99ac0606d27a9907771115
Paula Frias est <pau.frias@duocuc.cl>
2026-05-03 02:02:50 -0400

    feat: readme and setup

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ print("setup")
 # from setuptools import setup
 
 # setup(
-#    name='foo',
+#    name='RobusPredictor',
 #    version='1.0',
-#    description='A useful module',
-#    author='Man Foo',
-#    author_email='foomail@foo.example',
-#    packages=['foo'],  #same as name
-#    install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+#    description='Algoritmo predictivo para Formulisa',
+#    author='Sebastian y Paula',
+#    author_email='se.valdivia@duocuc.cl',
+#    packages=['RobusPredictor'],  #same as name
+#    install_requires=[
+#       "numpy==1.24.4",
+#       'pandas==2.0.3', 
+#       'scikit-learn==1.3.2', 
+#       'joblib==1.4.2'], #external packages as dependencies}
+#    python_requires="==3.8.10"
 # )
